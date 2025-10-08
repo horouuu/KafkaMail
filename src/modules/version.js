b4bb2c06e4344fd6ea6916bd21b1a6e4334ad323
horouuu <fernandafadja@gmail.com>
2025-10-08 13:15:28 +0800

    patch(version): remove update notifications from source repo

--- src/modules/version.js
+++ src/modules/version.js
@@ -14,13 +14,6 @@ module.exports = ({ bot, knex, config, commands }) => {
   commands.addInboxServerCommand("version", [], async (msg, args, thread) => {
     let response = `Modmail ${getPrettyVersion()}`;
 
-    if (config.updateNotifications) {
-      const availableUpdate = await updates.getAvailableUpdate();
-      if (availableUpdate) {
-        response += ` (version ${availableUpdate} available)`;
-      }
-    }
-
     utils.postSystemMessageWithFallback(msg.channel, thread, response);
   });
 };
