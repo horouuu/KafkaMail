aefa7fc032bfb02e95a91e78b1e20e84f99b1d08
horouuu <fernandafadja@gmail.com>
2025-10-09 02:38:42 +0800

    feat(reminders): fix db migration bug

--- src/data/migrations/20251008083559_add_reminders_table.js
+++ src/data/migrations/20251008083559_add_reminders_table.js
@@ -5,9 +5,9 @@
 exports.up = async function (knex) {
   await knex.schema.createTable("reminders", (table) => {
     table.increments("id");
-    table.string("thread_id").index();
-    table.string("author_id", 20).index();
-    table.datetime("fire_at");
+    table.string("thread_id", 36).notNullable().index();
+    table.string("author_id", 20).notNullable().index();
+    table.datetime("fire_at").notNullable();
     table.datetime("created_at");
   });
 };
@@ -16,4 +16,8 @@ exports.up = async function (knex) {
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function (knex) {};
+exports.down = async function (knex) {
+  if (await knex.schema.hasTable("reminders")) {
+    await knex.schema.dropTable("reminders");
+  }
+};
