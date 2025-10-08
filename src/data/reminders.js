0a6d6401ae4c9adf9fa81390bfd9d6b19f1e5d43
horouuu <fernandafadja@gmail.com>
2025-10-09 03:27:49 +0800

    feat(reminders): inserting and checking reminders

---
+++ src/data/reminders.js
@@ -0,0 +1,37 @@
+const knex = require("../knex");
+const moment = require("moment");
+
+/**
+ * Creates a new reminder row in the database
+ * @param {Object} data
+ * @returns {Promise<Object>} The ID of the created reminder
+ */
+
+async function createReminderInDB(data) {
+  const rem = {
+    ...data,
+    created_at: moment.utc().format("YYYY-MM-DD HH:mm:ss"),
+  };
+
+  if (!(await knex("reminders"))) {
+    console.log("Can't find table.");
+  }
+
+  const existing = await knex("reminders")
+    .where("thread_id", data.thread_id)
+    .select();
+
+  let out = null;
+
+  if (existing.length > 0) {
+    out = { existing: true, reminder: existing };
+  } else {
+    const id = await knex("reminders").insert(rem);
+    const created = await knex("reminders").where("id", id[0]).select();
+    out = { existing: false, reminder: created };
+  }
+
+  return out;
+}
+
+module.exports = { createReminderInDB };
--- src/modules/reminders.js
+++ src/modules/reminders.js
@@ -1,3 +1,11 @@
+const moment = require("moment");
+const Reminder = require("../data/Reminder");
+const { createReminderInDB } = require("../data/reminders");
+const {
+  parseDurationString,
+  getCountdownDurationFromMoment,
+} = require("../utils");
+
 module.exports = ({ bot, knex, config, commands }) => {
   function reminderCallback(thread) {}
 
@@ -6,16 +14,41 @@ module.exports = ({ bot, knex, config, commands }) => {
     "<duration:string$>",
 
     async (msg, args, thread) => {
-      const regex = new RegExp("^[0-9]+([smhd])$");
       const duration = args.duration;
-      if (!regex.test(duration)) {
+      let durationMs = 0;
+      try {
+        durationMs = parseDurationString(duration);
+      } catch (e) {
         thread.postSystemMessage(
-          `Invalid input for 'duration': \`${duration}\`.\nYou can only input a number followed by \`s\`, \`m\`, \`h\`, or \`d\`.\n\nExample: \`!reminder 7d\``
+          `Invalid input for 'duration': \`${duration}\`\nYou can only input a non-zero number followed by \`s\`, \`m\`, \`h\`, or \`d\`.\n\nExample: \`!reminder 7d\``
         );
         return;
       }
 
-      
+      const fireDate = new Date(new Date().getTime() + durationMs);
+      const rem = new Reminder({
+        thread_id: thread.id,
+        author_id: msg.author.id,
+        author_name: msg.author.globalName,
+        fire_at: moment.utc(fireDate).format("YYYY-MM-DD HH:mm:ss"),
+      });
+
+      const res = await createReminderInDB(rem);
+      const outRem = res.reminder[0];
+      if (res.existing) {
+        thread.postSystemMessage(
+          `There is already a reminder set for this channel:\n\nReminder in \`${getCountdownDurationFromMoment(
+            outRem.fire_at
+          )}\`\nCreated by \`${outRem.author_name}\` at \`${
+            outRem.created_at
+          }\` (UTC)`
+        );
+        return;
+      } else {
+        thread.postSystemMessage(
+          `Successfully set reminder for \`${outRem.fire_at}\` (\`${duration}\`).`
+        );
+      }
     },
     { allowSuspended: true }
   );
